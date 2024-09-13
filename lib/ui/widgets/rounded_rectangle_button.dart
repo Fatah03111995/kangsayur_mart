e944c17db287be7a766f4a8081544ecf2e7acb2b
Abdul Fatah <fatah03111995@gmail.com>
2024-09-13 10:52:59 +0700

    feat : add rounded rectangle button

---
+++ lib/ui/widgets/rounded_rectangle_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class RoundedRectangleButton extends StatelessWidget {
+  final void Function() onTap;
+  final Widget child;
+  final Color color;
+  final double height;
+  const RoundedRectangleButton({
+    super.key,
+    required this.onTap,
+    required this.child,
+    required this.color,
+    this.height = 50,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.w),
+        color: color,
+      ),
+      child: TextButton(onPressed: onTap, child: child),
+    );
+  }
+}
